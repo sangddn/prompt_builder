4ab37405506757f69c9c2844748f4c43a5510f87
Sang Doan <std35@georgetown.edu>
2024-12-25 23:42:51 -0800

    Update animations.dart to export state_animations.dart for enhanced animation capabilities

--- lib/src/components/animations/animations.dart
+++ lib/src/components/animations/animations.dart
@@ -2,4 +2,5 @@ export 'animated_state_provider.dart';
 export 'bouncing_object.dart';
 export 'hover_tap_builder.dart';
 export 'on_hover_widgets.dart';
+export 'state_animations.dart';
 export 'switchers.dart';
