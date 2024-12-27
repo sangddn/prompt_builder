af7547afa0da41add59eff20dd1dc870c14647c4
Sang Doan <std35@georgetown.edu>
2024-12-26 23:37:49 -0800

    Add LongHoverButton component for enhanced hover interactions
    - Introduced a new `LongHoverButton` widget that triggers a callback when the pointer hovers over it for longer than a specified duration.
    - Updated `buttons.dart` to export the new `LongHoverButton` component, integrating it into the existing button library.
    
    These changes enhance user interaction by providing a new way to respond to hover events, improving the overall usability of buttons in the application.

--- lib/src/components/buttons/buttons.dart
+++ lib/src/components/buttons/buttons.dart
@@ -2,6 +2,7 @@ export 'c_button.dart';
 export 'context_menu_button.dart';
 export 'copy_button.dart';
 export 'feedback_button.dart';
+export 'long_hover_button.dart';
 export 'menu_button.dart';
 export 'p_ink.dart';
 export 'tile_button.dart';
---
+++ lib/src/components/buttons/long_hover_button.dart
@@ -0,0 +1,68 @@
+import 'dart:async';
+
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+
+import '../../core/core.dart';
+
+/// A button that triggers a callback when the pointer hovers over it for longer
+/// than a specified duration.
+class LongHoverButton extends StatefulWidget {
+  const LongHoverButton({
+    this.longHoverThreshold = Effects.longDuration,
+    this.onHover,
+    this.onExit,
+    this.onLongHover,
+    required this.child,
+    super.key,
+  });
+
+  /// The duration after which the [onLongHover] callback is called.
+  final Duration longHoverThreshold;
+
+  /// The callback that is called when the pointer hovers over the button.
+  final void Function(PointerHoverEvent)? onHover;
+
+  /// The callback that is called when the pointer exits the button.
+  final void Function(PointerExitEvent)? onExit;
+
+  /// The callback that is called when the pointer hovers over the button for
+  /// longer than [longHoverThreshold].
+  final void Function(PointerHoverEvent)? onLongHover;
+
+  /// The child widget.
+  final Widget child;
+
+  @override
+  State<LongHoverButton> createState() => _LongHoverButtonState();
+}
+
+class _LongHoverButtonState extends State<LongHoverButton> {
+  Timer? _longHoverTimer;
+
+  void _handleHoverStart(PointerHoverEvent event) {
+    widget.onHover?.call(event);
+    _longHoverTimer?.cancel();
+    _longHoverTimer = Timer(widget.longHoverThreshold, () {
+      widget.onLongHover?.call(event);
+    });
+  }
+
+  void _handleHoverEnd(PointerExitEvent event) {
+    widget.onExit?.call(event);
+    _longHoverTimer?.cancel();
+  }
+
+  @override
+  void dispose() {
+    _longHoverTimer?.cancel();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) => MouseRegion(
+        onHover: _handleHoverStart,
+        onExit: _handleHoverEnd,
+        child: widget.child,
+      );
+}
